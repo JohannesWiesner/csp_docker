ee04a216d843d997e4537957b62efea1521ac155
JohannesWiesner <joh.wiesner@gmail.com>
2023-02-09 15:34:00 +0100

    pwd has to be uppercase so we expand to needed absolute path

--- test_container.sh
+++ test_container.sh
@@ -1,7 +1,7 @@
 docker run -ti --rm \
--v ${pwd}/testing/code:/home/csp/code \
--v ${pwd}/testing/data:/home/csp/data \
--v ${pwd}/testing/output:/home/csp/output \
+-v ${PWD}/testing/code:/home/csp/code \
+-v ${PWD}/testing/data:/home/csp/data \
+-v ${PWD}/testing/output:/home/csp/output \
 -p 8888:8888 \
 csp:test
 
