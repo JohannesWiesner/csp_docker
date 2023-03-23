9c515fc387c7056ed20fde0767a181df48cde683
JohannesWiesner <joh.wiesner@gmail.com>
2023-03-23 12:20:46 +0100

    added bash script that allows you to download test data using openneuro-py

---
+++ download_test_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+openneuro-py download \
+--dataset ds004302 \
+--include=sub-01/anat/sub-01_T1w.nii.gz \
+--include=sub-01/func/sub-01_task-speech_bold.nii.gz \
+--target_dir=testing/data/tmp &&
+cp testing/data/tmp/sub-01/anat/sub-01_T1w.nii.gz testing/data/anatomical.nii.gz &&
+cp testing/data/tmp/sub-01/func/sub-01_task-speech_bold.nii.gz testing/data/functional.nii.gz &&
+rm -r testing/data/tmp
