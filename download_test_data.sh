cfab0eee29d6ea6db18db11bfbd16ef28d8e18bb
JohannesWiesner <joh.wiesner@gmail.com>
2023-04-27 11:57:24 +0200

    improved this script: only download ones and then extract and unzip to different filenames

--- download_test_data.sh
+++ download_test_data.sh
@@ -6,25 +6,7 @@
 # See: https://github.com/hoechenberger/openneuro-py#installation
 
 ###############################################################
-# 1.) Download an anatomical and functional file from openneuro
-# We will use this for very basic tests that do not
-# require BIDS-conformity. We will unzip the images because
-# SPM can't handle .gz files.
-###############################################################
-
-openneuro-py download \
---dataset ds004302 \
---include=sub-01/anat/sub-01_T1w.nii.gz \
---include=sub-01/func/sub-01_task-speech_bold.nii.gz \
---target_dir=testing/data/tmp &&
-cp testing/data/tmp/sub-01/anat/sub-01_T1w.nii.gz testing/data/anatomical.nii.gz &&
-cp testing/data/tmp/sub-01/func/sub-01_task-speech_bold.nii.gz testing/data/functional.nii.gz &&
-rm -r testing/data/tmp &&
-gzip -d testing/data/anatomical.nii.gz &&
-gzip -d testing/data/functional.nii.gz
-
-###############################################################
-# 2.) Download a BIDS-dataset with only one subject. With this
+# 1.) Download a BIDS-dataset with only one subject. With this
 # we can test applications that require BIDS-conformity
 ###############################################################
 
@@ -32,3 +14,14 @@ openneuro-py download \
 --dataset ds004302 \
 --include=sub-01 \
 --target_dir=testing/data/bids
+
+###############################################################
+# 2.) We will unzip the images because SPM can't handle .gz files.
+###############################################################
+
+mkdir testing/data/single_files && \
+gzip -dc testing/data/bids/sub-01/anat/sub-01_T1w.nii.gz > testing/data/single_files/anatomical.nii && \
+gzip -dc testing/data/bids/sub-01/func/sub-01_task-speech_bold.nii.gz > testing/data/single_files/functional.nii
+
+
+
