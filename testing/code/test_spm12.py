f44b13b60be467c7f21a38bf646060b423022d04
JohannesWiesner <joh.wiesner@gmail.com>
2023-05-02 11:39:23 +0200

    changed paths

--- testing/code/test_spm12.py
+++ testing/code/test_spm12.py
@@ -1,5 +1,5 @@
 from nipype.interfaces.spm import Smooth
 smooth = Smooth()
-smooth.inputs.in_files = '/data/functional.nii'
+smooth.inputs.in_files = '/home/csp/data/single_files/functional.nii'
 smooth.inputs.fwhm = 6
 smooth.run()
